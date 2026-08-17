1862271e529107ad3b7e1e1e19386bc8d20df224
YLong <YLong@233.com>
2026-08-17 10:56:41 +0800

    初始化：后端backend + 前端frontend

---
+++ demotestFront/src/test/java/com/demo/demotestfront/DemotestFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.demotestfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotestFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
